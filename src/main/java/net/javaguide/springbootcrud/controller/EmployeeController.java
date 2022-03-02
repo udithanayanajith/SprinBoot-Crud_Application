d8a98610f307747ba17fe35dd57c66dc978935c1
udithanayanajith <uditha.nayanagith@gmail.com>
2022-03-02 15:05:42 +0530

    Implemented the employee get api

--- src/main/java/net/javaguide/springbootcrud/controller/EmployeeController.java
+++ src/main/java/net/javaguide/springbootcrud/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package net.javaguide.springbootcrud.controller;
 import net.javaguide.springbootcrud.model.Employee;
 import net.javaguide.springbootcrud.repository.EmployeeRepositary;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,12 @@ public class EmployeeController {
     public List<Employee> getAllEmployyes(){
 
         return employeeRepositary.findAll();
+    }
+
+    //Create a new employee
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepositary.save(employee);
     }
 
 }
