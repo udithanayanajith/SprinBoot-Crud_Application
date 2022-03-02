545887c1ada147c6f3bd0b2abbfeea4b047e2359
udithanayanajith <uditha.nayanagith@gmail.com>
2022-03-02 14:16:18 +0530

    Initialized the app

---
+++ src/main/java/net/javaguide/springbootcrud/model/Employee.java
@@ -0,0 +1,23 @@
+package net.javaguide.springbootcrud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer contact;
+    private String address;
+}
