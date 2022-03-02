6bde1515883a15f2ece36ea27b9eea5f8055a2db
udithanayanajith <uditha.nayanagith@gmail.com>
2022-03-02 14:57:39 +0530

    Implemented the employee controller

--- src/main/java/net/javaguide/springbootcrud/SpringbootCrudApplication.java
+++ src/main/java/net/javaguide/springbootcrud/SpringbootCrudApplication.java
@@ -1,5 +1,7 @@
 package net.javaguide.springbootcrud;
 
+import net.javaguide.springbootcrud.repository.EmployeeRepositary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCrudApplication.class, args);
-	}
+		SpringApplication.run(SpringbootCrudApplication.class, args);}
+	@Autowired
+	private EmployeeRepositary employeeRepositary;
 
 }
--- src/main/java/net/javaguide/springbootcrud/controller/EmployeeController.java
+++ src/main/java/net/javaguide/springbootcrud/controller/EmployeeController.java
@@ -3,16 +3,23 @@ package net.javaguide.springbootcrud.controller;
 import net.javaguide.springbootcrud.model.Employee;
 import net.javaguide.springbootcrud.repository.EmployeeRepositary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepositary employeeRepositary;
+
+
     //Get employee list methode
+    @GetMapping
     public List<Employee> getAllEmployyes(){
+
         return employeeRepositary.findAll();
     }
 
