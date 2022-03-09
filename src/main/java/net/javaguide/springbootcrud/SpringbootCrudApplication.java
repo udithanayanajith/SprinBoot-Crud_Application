0780e338f775ebcc53d54f03599139c6855a81f8
udithanayanajith <uditha.nayanagith@gmail.com>
2022-03-09 16:45:28 +0530

    Changed the cross origin anotation

--- src/main/java/net/javaguide/springbootcrud/SpringbootCrudApplication.java
+++ src/main/java/net/javaguide/springbootcrud/SpringbootCrudApplication.java
@@ -4,6 +4,7 @@ import net.javaguide.springbootcrud.repository.EmployeeRepositary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SpringbootCrudApplication {
